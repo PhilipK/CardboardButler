15cf7d5932744dd6f99229cb3f0a686ddb9e9e52
Philip Kristoffersen <philipkristoffersen@gmail.com>
2019-09-05 07:17:57 +0200

    Rendering the year.

--- tests/services/components/GameItem.test.tsx
+++ tests/services/components/GameItem.test.tsx
@@ -1,3 +1,4 @@
+
 import * as React from "react";
 import GameListItem from "../../../src/components/GameListItem";
 import { GameInfo } from "../../../src/models/GameInfo";
@@ -10,26 +11,33 @@ describe("GameItem react component", () => {
 
     const testItem: GameInfo = alchemists;
 
-    it("is rendered given a gameinfo item", () => {
-        const { getByTestId } = render(<GameListItem item={testItem} />);
-        expect(getByTestId("GameName")).toBeDefined();
-    });
+    describe("it renders", () => {
+        it(" a gameinfo item", () => {
+            const { getByTestId } = render(<GameListItem item={testItem} />);
+            expect(getByTestId("GameName")).toBeDefined();
+        });
 
-    it("renders the game name", () => {
-        const { getByTestId } = render(<GameListItem item={testItem} />);
-        expect(getByTestId("GameName")).toHaveTextContent(testItem.name);
-    });
+        it("name", () => {
+            const { getByTestId } = render(<GameListItem item={testItem} />);
+            expect(getByTestId("GameName")).toHaveTextContent(testItem.name);
+        });
 
-    it("renders the game image", () => {
-        const { getByTestId } = render(<GameListItem item={testItem} />);
-        expect(getByTestId("GameImage")).toHaveAttribute("src", testItem.imageUrl);
-    });
+        it("image", () => {
+            const { getByTestId } = render(<GameListItem item={testItem} />);
+            expect(getByTestId("GameImage")).toHaveAttribute("src", testItem.imageUrl);
+        });
 
-    it("renders the game description", () => {
-        const descriptionGenerator = new DescriptionGenerator();
-        const expected = descriptionGenerator.generateDescription(testItem);
-        const { getByTestId } = render(<GameListItem item={testItem} />);
-        expect(getByTestId("GameDescription")).toHaveTextContent(expected);
+        it("description", () => {
+            const descriptionGenerator = new DescriptionGenerator();
+            const expected = descriptionGenerator.generateDescription(testItem);
+            const { getByTestId } = render(<GameListItem item={testItem} />);
+            expect(getByTestId("GameDescription")).toHaveTextContent(expected);
+        });
+
+        it("year published", () => {
+            const { getByTestId } = render(<GameListItem item={testItem} />);
+            expect(getByTestId("GameYear")).toHaveTextContent(testItem.yearPublished + "");
+        });
     });
 });
 
