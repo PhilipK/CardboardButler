166307311b21ddeecdddec1bdec6eff430ccd663
Philip Kristoffersen <philipkristoffersen@gmail.com>
2019-09-08 13:33:41 +0200

    Refactoring.

--- src/components/ValidatingUserInput.tsx
+++ src/components/ValidatingUserInput.tsx
@@ -32,44 +32,58 @@ export default class ValidatingUserInput extends React.Component<Props, State> {
         super(props);
         this.onNamesChange = this.onNamesChange.bind(this);
         this.state = initialState;
+        this.doesNameNeedValidation = this.doesNameNeedValidation.bind(this);
+        this.isNameShown = this.isNameShown.bind(this);
     }
 
 
+    doesNameNeedValidation(name: string): boolean {
+        const { validNames, invalidNames, loadingNames } = this.state;
+        const { userValidator } = this.props;
+        if (!userValidator) {
+            return false;
+        }
+        return !(validNames.indexOf(name) > -1)
+            && !(invalidNames.indexOf(name) > -1)
+            && !(loadingNames.indexOf(name) > -1);
+    }
+
+    isNameShown(name: string): boolean {
+        return this.state.shownNames.indexOf(name) > -1;
+    }
 
     onNamesChange(names: string[]) {
-        const validate = this.props.userValidator;
-        const knownNames = [...this.state.validNames, ...this.state.invalidNames, ...this.state.loadingNames];
-        const namesToValidate = validate ? names.filter((name) => knownNames.indexOf(name) === -1) : [];
         this.setState({
             shownNames: names,
         });
-        if (validate) {
-            namesToValidate.forEach(async (name) => {
-                setTimeout(async () => {
+        const needValidate = this.doesNameNeedValidation;
+        const namesToValidate = names.filter(needValidate);
+        namesToValidate.forEach(async (name) => {
+            setTimeout(async () => {
+                //things might have changed, so check again if the name is still shown
+                if (this.isNameShown(name) && needValidate(name)) {
+                    const { userValidator } = this.props;
                     const loadingNow = this.state.loadingNames;
-                    const alreadyLoading = loadingNow.indexOf(name) > -1;
-                    const stillShowingName = this.state.shownNames.indexOf(name) > -1;
-                    if (stillShowingName && !alreadyLoading) {
+                    this.setState({
+                        loadingNames: [...loadingNow, name]
+                    })
+                    const isValid = await userValidator(name);
+                    const loadingWithoutName = this.state.loadingNames.filter((loadingName) => loadingName !== name)
+                    if (isValid) {
                         this.setState({
-                            loadingNames: [...loadingNow, name]
+                            validNames: [...this.state.validNames, name],
+                            loadingNames: loadingWithoutName
                         })
-                        const isValid = await validate(name);
-                        const loadingWithoutName = this.state.loadingNames.filter((loadingName) => loadingName !== name)
-                        if (isValid) {
-                            this.setState({
-                                validNames: [...this.state.validNames, name],
-                                loadingNames: loadingWithoutName
-                            })
-                        } else {
-                            this.setState({
-                                invalidNames: [...this.state.invalidNames, name],
-                                loadingNames: loadingWithoutName
-                            })
-                        }
+                    } else {
+                        this.setState({
+                            invalidNames: [...this.state.invalidNames, name],
+                            loadingNames: loadingWithoutName
+                        })
                     }
-                }, 200);
-            });
-        };
+                }
+                //wait a little with calling validator, to make sure people are done typing.
+            }, 200);
+        });
     }
 
     render() {
