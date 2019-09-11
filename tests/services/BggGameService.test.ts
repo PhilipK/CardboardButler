fe64f2d01b0031599fe5122d77f70fe38fd15795
Philip Kristoffersen <philipkristoffersen@gmail.com>
2019-09-11 07:32:01 +0200

    Linting

--- tests/services/BggGameService.test.ts
+++ tests/services/BggGameService.test.ts
@@ -3,7 +3,7 @@ import * as fetchMock from "fetch-mock";
 import { readFileSync } from "fs";
 
 describe("BggGameService", () => {
-    
+
     const fetch = fetchMock.sandbox();
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
